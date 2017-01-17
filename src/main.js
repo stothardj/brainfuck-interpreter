f84e42580941738626971ee612367a9e1bb5f342
Jake Stothard <stothardj@gmail.com>
2017-01-16 23:33:57 -0800

    Add heuristic for inputting characters to use ascii value for the data

--- src/main.js
+++ src/main.js
@@ -3,14 +3,17 @@ import {Interpreter} from "./interpreter";
 const codeform = document.getElementById('codeform');
 const outdiv = document.getElementById('output');
 
+function simpleAsciiValue(data) {
+  return data > 96 && data < 123 // uppercase letters
+    || data > 64 && data < 91 // lowercase letters
+    || data > 31 && data < 48 // symbols. stop before numbers since that would be confusing
+    || data > 8 && data < 12; // whitespace
+}
+
 function output(data) {
   let out;
   // Allow certain portions of the ascii table, otherwise display data as a number
-  if (data > 96 && data < 123 // uppercase letters
-    || data > 64 && data < 91 // lowercase letters
-    || data > 31 && data < 48 // symbols. stop before numbers since that would be confusing
-    || data > 8 && data < 12 // whitespace
-  ) {
+  if (simpleAsciiValue(data)) {
     out = String.fromCharCode(data);
   } else {
     out = data;
@@ -20,6 +23,13 @@ function output(data) {
 
 function input() {
   const input = window.prompt('Input value for script');
+  // Translate single character into its ascii value
+  if (input.length == 1) {
+    const val = input.charCodeAt(0);
+    if (simpleAsciiValue(val)) {
+      return val;
+    }
+  }
   return parseInt(input);
 }
 
